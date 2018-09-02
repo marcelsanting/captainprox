a2b0cc8501e728c91ff2d17b4be06d0d9802330b
Marcel Santing <marcel@prox-web.nl>
2018-09-02 14:30:01 +0200

    Started with the statuses functionality

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -1,9 +1,42 @@
 <?php
-
+/**
+ * ProjectController
+ *
+ * ProjectController gives all projects, features and tasks their current status
+ *
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to the MIT license
+ * that is available through the world-wide-web at the following URI:
+ * https://opensource.org/licenses/MIT
+ *
+ * @category  LaravelProject
+ * @package   ProxCMS
+ * @author    Original Author <author@example.com>
+ * @author    Marcel Santing <marcel@prox-web.nl>
+ * @copyright 2018 Prox-Web
+ * @license   https://opensource.org/licenses/MIT  MIT License
+ * @version   SVN: $Id$
+ * @link      https://github.com/marcelsanting/captainprox
+ * @since     File available since Release 1.0.0
+ */
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * Class ProjectController
+ *
+ * Handles view and models of the Projects
+ *
+ * @category  Controller
+ * @package   App\Http\Controllers
+ * @author    Original Author <author@example.com>
+ * @author    Marcel Santing <marcel@prox-web.nl>
+ * @copyright 2018 Prox-Web
+ * @license   https://opensource.org/licenses/MIT  MIT License
+ * @link      https://github.com/marcelsanting/captainproxessionController
+ */
 class ProjectController extends Controller
 {
     //
