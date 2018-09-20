38b012849c358f7911977dc27ae46b81804bea71
Marcel Santing <marcel@prox-web.nl>
2018-09-20 14:18:40 +0200

    Added doc comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -1,11 +1,41 @@
 <?php
-
+/**
+ * CommentController
+ *
+ * CommentController sets the basis after the user has logged on to the website
+ *
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to the MIT license
+ * that is available through the world-wide-web at the following URI:
+ * https://opensource.org/licenses/MIT
+ *
+ * @category  LaravelProject
+ * @package   ProxCMS
+ * @author    Original Author <author@example.com>
+ * @author    Marcel Santing <marcel@prox-web.nl>
+ * @copyright 2018 Prox-Web
+ * @license   https://opensource.org/licenses/MIT  MIT License
+ * @version   SVN: $Id$
+ * @link      https://github.com/marcelsanting/captainprox
+ * @since     File available since Release 1.0.0
+ */
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCommentPost;
 use App\Models\TaskComment;
 
-
+/**
+ * Class CommentController
+ *
+ * @category  Controller
+ * @package   App\Http\Controllers
+ * @author    Original Author <author@example.com>
+ * @author    Marcel Santing <marcel@prox-web.nl>
+ * @copyright 2018 Prox-Web
+ * @license   https://opensource.org/licenses/MIT  MIT License
+ * @link      https://github.com/marcelsanting/captainprox
+ */
 class CommentController extends Controller
 {
     /**
